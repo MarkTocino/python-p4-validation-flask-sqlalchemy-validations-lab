21e5ef6b79390d94d45a64a8c91c7313d77ec1c2
MarkTocino <Mark_tocino@yahoo.com>
2023-08-19 14:20:47 -0400

    Completed Validation Flask

--- server/models.py
+++ server/models.py
@@ -12,6 +12,17 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name is needed')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone Number must be 10 digits")
+        return phone_number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +37,29 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    @validates('title')
+    def validate_title(self, key, title):
+        title_must = ["Won't Believe", "Secret", "Top", "Guess"]
+        for x in title_must:
+            if x in title:
+                return title
+        else:
+            raise ValueError('Title must be a clickbait')
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be 250 characters long')
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError('Summary is too long')
+        return summary
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Category must be Fiction or Non-Fiction')
+        return category
 
 
     def __repr__(self):
